ee849f1d8f703db1378af819922cbf0c75e0e7b9
Faxiis <antho.lavoute@gmail.com>
2025-09-17 16:13:10 +0200

    Rate limit added

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import cors from "cors";
 import type { Request, Response } from "express";
 import authRouter from "./routes/auth.js";
 import usersRouter from "./routes/users.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 3000;
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limite chaque IP à 100 requêtes par fenêtre
+    standardHeaders: true,
+    legacyHeaders: false,
+});
 
 // Middleware
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 
